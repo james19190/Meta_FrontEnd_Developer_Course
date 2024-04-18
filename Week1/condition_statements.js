174e103bdcd444d4c6bd06f499f624c6ae96b746
James Kyoung Ho Kim <james19190@gmail.com>
2024-04-18 15:01:54 +0900

    git commit -m "Week 1 - JavaScript Basics"

---
+++ Week1/condition_statements.js
@@ -0,0 +1,12 @@
+//Are you old enough
+
+var age = 70;
+if (age >= 65){
+    console.log("You get your income from your pension");
+}
+else if (age <= 65 && age >= 18){
+    console.log("Each month you get your salary");
+}
+else if (age < 18){
+    console.log("You get your pocket money from your parents");
+}
---
+++ Week1/operators.js
@@ -0,0 +1,26 @@
+//Task 1
+
+var score = 8;
+
+console.log("Mid level score:", score > 0 && score < 10);
+
+
+//Task 2
+var timeRemaining = 0;
+var energy = 10;
+
+console.log("Game over:", timeRemaining === 0 || energy === 0); 
+
+//Task 3
+
+var num1 = 2;
+var num2 = 5;
+
+var test1 = num1%2;
+var test2 = num2%2;
+
+var result1 = test1 == 0;
+var result2 = test2 == 0;
+
+console.log("Number 1 is even:", result1);
+console.log("Number 2 is even:", result2);
