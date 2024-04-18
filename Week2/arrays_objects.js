b38b8d97cc6b1c315ded59923a477e1c9e3058f1
James Kyoung Ho Kim <james19190@gmail.com>
2024-04-18 15:25:57 +0900

    Week 2 - Arrays Objects and Functions

---
+++ Week2/arrays_objects.js
@@ -0,0 +1,15 @@
+var clothes = [];
+clothes.push('shirt');
+clothes.push('pants');
+clothes.push('socks');
+clothes.push('scarf');
+clothes.push('hat');
+clothes.pop();
+clothes.push('gloves');
+console.log(clothes[2]);
+
+var favCar = {};
+
+favCar.color = 'black';
+favCar.convertible = true;
+console.log(favCar);
